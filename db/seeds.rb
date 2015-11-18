437ab27edce2796248a5cad07c279db28c226b9a
Eduard Bagdasaryan <dictiano@gmail.com>
2015-11-18 16:23:12 +0300

    Стандартное заполнение тестовыми данными

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = %W(Иванов Петров Сидоров Нельсон_Мандела Трус Балбес Бывалый)
+i = 1
+users.each do |u|
+  tok = "tok_#{u}"
+  user = User.create(name: u, token: tok)
+  user.save
+  i.times do
+    v = rand(1..5)
+    twit = user.twits.create(message: "#{u}_message_#{i}", vote: v)
+    twit.save
+  end
+  i += 1
+end
